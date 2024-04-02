37f28dcf37c52de5ab2765958e91d50021ade90b
tom <anytable@gmail.com>
2024-04-02 22:01:20 +0800

    ~

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,24 +1,12 @@
 import {handleUpgrade} from "vite-sveltekit-cf-ws";
 
-let once = 0
-function init(){
-    if (once) return
-    once = 1
-    handleUpgrade((req, createWebsocketServer) => {
-        const pathname = new URL(req.url || '', 'ws://base.url').pathname
-        if (pathname === '/hello') {
-            const server = createWebsocketServer()
-            server.accept()
-            server.addEventListener('message', ({data}) => {
-                server.send(`[ws server] received message: "${data}"`)
-            })
-        }
-    })
-}
-
-
-
-export const handle = ({event, resolve}) => {
-    init()
-    return resolve(event)
-}
+handleUpgrade((req, createWebsocketServer) => {
+    const pathname = new URL(req.url || '', 'ws://base.url').pathname
+    if (pathname === '/hello') {
+        const server = createWebsocketServer()
+        server.accept()
+        server.addEventListener('message', ({data}) => {
+            server.send(`[ws server] received message: "${data}"`)
+        })
+    }
+})
